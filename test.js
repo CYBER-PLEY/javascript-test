77a83e0534dbd057746f47f5bd3e37cb55ad3f08
Prince Lawson <princelawson691@gmail.com>
2023-12-21 11:00:56 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,77 @@
+// // // // // // // // const para = document.querySelector("h1")
+// // // // // // // // console.log(para)
+
+// // // // // // // let email = "princelawson691@gmail.com";
+// // // // // // // let firstName= "Prince ";
+// // // // // // // let lastName= "Lawson ";
+// // // // // // // let fullName= firstName + " " + lastName;
+// // // // // // // console.log( fullName ) ;
+// // // // // // // console.log( fullName.length);
+// // // // // // // console.log(fullName.toUpperCase());
+// // // // // // // // let result= email.substring(3,9);
+// // // // // // // console.log(result)
+
+// // // // // // let pi=3.14;
+// // // // // // let radius=10;
+// // // // // // console.log(radius , pi)
+// // // // // // console.log(radius / 2)
+// // // // // // console.log(pi * 2.4)
+// // // // // // let likes = 10;
+// // // // // // likes++
+// // // // // // console.log(likes)
+// // // // // let spy = ["dream", 124, "height"]
+// // // // // console.log(spy[2])
+// // // // // let result = spy.includes("dream");
+// // // // // console.log(result)
+
+// // // // // // let result= "The blog has " + likes + " likes"
+// // // // // // console.log(result)
+// // // // // // const title = "Prince"
+// // // // // // let result= `${title} has a big brain`
+// // // // // // console.log(result)
+
+// // // // let age = 25 
+// // // // console.log(age== 25)
+// // // // console.log(age=="25")
+// // // // console.log(age===25)
+// // // // // console.log(age==="25")
+// // // // for (let i=0 ; i<10; i++){
+// // // //     console.log(i)
+    
+// // // // }
+// // // let i=0;
+// // // while(i<5){
+// // //     console.log('in loop: ', i)
+// // //     ; i++
+// // // }
+// // const password = "pas@s1111111";
+
+// // if (password.length >= 8 && password.includes("@")){
+// //     console.log("password is long enough .");
+// // }
+// // else{
+// //     console.log("password is not long enough");
+// // }
+// // // else if
+//  const greet = () => "hello world";
+//  const result= greet();
+//  console.log(result)
+
+//  const bill = (products , tax ) =>{
+//     return 
+//  }
+
+ let people = ["grace" , "love" ,"peace" , "joy"];
+ let i = 0;
+
+ people.forEach(function(person){
+    console.log(person)
+ });
+ while (people.length >= i){
+    console.log("mehhhn")
+    i++
+ };
+
+ const para= document.querySelector("p")
+ console.log(para[2]);
+ para.innerText += " hi bro"
